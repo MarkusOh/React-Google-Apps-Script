ebcee26f38f8091f76fdb109f732e3188acf577a
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-08-10 23:45:25 +0900

    Boilerplate cleanup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,6 @@
 import {
   onOpen,
-  openDialog,
-  openDialogBootstrap,
-  openDialogMUI,
   openDialogTailwindCSS,
-  openAboutSidebar,
 } from './ui';
 
 import { getSheetsData, addSheet, deleteSheet, setActiveSheet, getData, getAllSchedules, getAllAssociatedSchedules, getAllAssociatedSchedulesForName, getAllSchedulesForSlot, getAllSlots } from './sheets';
@@ -12,11 +8,7 @@ import { getSheetsData, addSheet, deleteSheet, setActiveSheet, getData, getAllSc
 // Public functions must be exported as named exports
 export {
   onOpen,
-  openDialog,
-  openDialogBootstrap,
-  openDialogMUI,
   openDialogTailwindCSS,
-  openAboutSidebar,
   getSheetsData,
   addSheet,
   deleteSheet,
--- src/server/ui.js
+++ src/server/ui.js
@@ -1,44 +1,14 @@
 export const onOpen = () => {
   const menu = SpreadsheetApp.getUi()
-    .createMenu('My Sample React Project') // edit me!
-    .addItem('Sheet Editor', 'openDialog')
-    .addItem('Sheet Editor (Bootstrap)', 'openDialogBootstrap')
-    .addItem('Sheet Editor (MUI)', 'openDialogMUI')
-    .addItem('Sheet Editor (Tailwind CSS)', 'openDialogTailwindCSS')
-    .addItem('About me', 'openAboutSidebar');
+    .createMenu('Auto Scheduler')
+    .addItem('Schedule Maker', 'openDialogTailwindCSS')
 
   menu.addToUi();
-};
-
-export const openDialog = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
-    .setWidth(800)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor');
-};
-
-export const openDialogBootstrap = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
-    .setWidth(800)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
-};
-
-export const openDialogMUI = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-mui')
-    .setWidth(800)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (MUI)');
 };
 
 export const openDialogTailwindCSS = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-tailwindcss')
     .setWidth(800)
     .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Tailwind CSS)');
-};
-
-export const openAboutSidebar = () => {
-  const html = HtmlService.createHtmlOutputFromFile('sidebar-about-page');
-  SpreadsheetApp.getUi().showSidebar(html);
+  SpreadsheetApp.getUi().showModalDialog(html, 'Schedule Maker');
 };
