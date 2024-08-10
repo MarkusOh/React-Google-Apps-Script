8fbbd9a7d8396c4e0af8a6137455209855f48199
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-08-10 22:50:19 +0900

    Width adjustment

--- src/server/ui.js
+++ src/server/ui.js
@@ -12,28 +12,28 @@ export const onOpen = () => {
 
 export const openDialog = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
-    .setWidth(600)
+    .setWidth(800)
     .setHeight(600);
   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor');
 };
 
 export const openDialogBootstrap = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
-    .setWidth(600)
+    .setWidth(800)
     .setHeight(600);
   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
 };
 
 export const openDialogMUI = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-mui')
-    .setWidth(600)
+    .setWidth(800)
     .setHeight(600);
   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (MUI)');
 };
 
 export const openDialogTailwindCSS = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-tailwindcss')
-    .setWidth(600)
+    .setWidth(800)
     .setHeight(600);
   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Tailwind CSS)');
 };
